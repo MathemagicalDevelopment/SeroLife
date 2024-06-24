7cebb8e36b010185a045f413e9a601b568123263
MathemagicalDevelopment <development@mathemagicalconsulting.co.uk>
2024-06-24 19:41:19 +0100

    create models

---
+++ api/models/recipe.model.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const ingredientSchema = new Schema({
+    name: { type: String, required: true },
+    qty: { type: Number, required: true },
+    unit: { type: String, required: true }
+})
+
+const recipeSchema = new Schema({
+    user_id: { type: Schema.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    created: { type: Date, required: true, default: Date.now },
+    ingredients: [ingredientSchema],
+    method: { type: String, required: true }
+})
+
+export const Recipe = mongoose.model('Recipe', recipeSchema)
---
+++ api/models/user.model.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    created: { type: Date, required: true, default: Date.now },
+    user_recipes: [{ type: Schema.ObjectId, ref: 'Recipe' }],
+    favourites: [{ type: Schema.ObjectId, ref: 'Recipe' }],
+});
+
+export const User = mongoose.model('User', userSchema);
