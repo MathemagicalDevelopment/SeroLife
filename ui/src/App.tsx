d06707658b5f135601d7baff41a0cd4f96634608
MathemagicalDevelopment <development@mathemagicalconsulting.co.uk>
2024-06-25 13:11:59 +0100

    handle routing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,15 +3,29 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home/Home";
-
+import LoginPage from "./pages/Login";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import { UserContextProvider } from "./contexts/user.context";
+import SignUpPage from './pages/SignUp'
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
+    <div className="page-container">
+      <UserContextProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/login">
+              <LoginPage />
+            </Route>
+            <Route exact path="/sign-up">
+              <SignUpPage />
+            </Route>
+            <AuthenticatedRoute path="/">
+              <Home />
+            </AuthenticatedRoute>
+          </Switch>
+        </Router>
+      </UserContextProvider>
+
     </div>
   );
 };
