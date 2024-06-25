0117e7f5f482ae1a52802be3f88550f66a37542b
MathemagicalDevelopment <development@mathemagicalconsulting.co.uk>
2024-06-25 13:29:06 +0100

    search by name

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -11,7 +11,9 @@ import RecipeForm from "../../components/forms/RecipeForm";
 
 export const Home = () => {
   const [loading, setLoading] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>('');
   const [userRecipes, setUserRecipes] = useState<Recipe[] | undefined>();
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[] | undefined>();
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const token = useToken();
   const user = useUser();
@@ -36,11 +38,33 @@ export const Home = () => {
     setIsVisible(true);
   }
 
+  const updateSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event, event.target, event.target.value);
+    setSearchValue(event.target.value)
+  }
+
+  useEffect(() => {
+    if (searchValue && userRecipes && searchValue.length > 0) {
+      setFilteredRecipes(userRecipes.filter(({ name }) => name.toLowerCase().includes(searchValue.toLowerCase())));
+    } else {
+      setFilteredRecipes(userRecipes);
+    }
+  }, [searchValue, userRecipes])
+
+
   return (
     <div>
       <Modal isVisible={isVisible} handleClose={() => setIsVisible(false)}><RecipeForm handleClose={() => { setIsVisible(false); handleStartUp() }} /></Modal>
       <div className="row center">
         <h1>Recipe App</h1>
+      </div>
+      <div className="row center">
+        <form>
+          <label>
+            Recipe name search:
+            <input type="search" value={searchValue} onChange={updateSearch} placeholder="Recipe name search" />
+          </label>
+        </form>
       </div>
       <div className="row center">
         <button onClick={showCreateRecipe}>Create Recipe</button>
@@ -52,8 +76,8 @@ export const Home = () => {
               <Spinner />
             </div> : <></>
         }
-        {!loading && userRecipes && userRecipes.length > 0 ?
-          userRecipes.map((r: Recipe) => <RecipeCard key={r._id} {...r} />)
+        {!loading && filteredRecipes && filteredRecipes.length > 0 ?
+          filteredRecipes.map((r: Recipe) => <RecipeCard key={r._id} {...r} />)
           :
           <div className="row center"><span>No recipes to show</span></div>}
       </div>
